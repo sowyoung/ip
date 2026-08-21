271ff98dcb8f5cfc6c1e1c6785b22df2946c132c
sowyoung <tansowyoung@gmail.com>
2026-08-21 12:18:42 +0800

    rename, greet, exit

---
+++ src/main/java/TungTung.java
@@ -0,0 +1,19 @@
+public class TungTung {
+    public static void main(String[] args) {
+        String space = "_____________________________________________________________";
+        String banner = "  _____          _    _ ______ _____  ______ \n"
+                + " / ____|   /\\   | |  | |  ____|  __ \\|  ____|\n"
+                + "| (___    /  \\  | |__| | |__  | |__) | |__   \n"
+                + " \\___ \\  / /\\ \\ |  __  |  __| |  _  /|  __|  \n"
+                + " ____) |/ ____ \\| |  | | |____| | \\ \\| |____ \n"
+                + "|_____//_/    \\_\\_|  |_|______|_|  \\_\\______|\n";
+        String greet = "Hello! Tung Tung Sahere!\nHow can I help you?";
+        String exit = "Bye! Tung Tung Nothere!";
+
+        System.out.println(banner);
+        System.out.println(greet);
+        System.out.println(space);
+        System.out.println(exit);
+        System.out.println(space);
+    }
+}
