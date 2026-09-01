44a898d615d88035b97ba90db28599d5c8ae3853
sowyoung <tansowyoung@gmail.com>
2026-09-01 10:16:47 +0800

    Document task classes
    Public task classes and state-changing methods lack descriptive Javadoc. Add headers so their intended behavior is clear to readers and maintainers.

--- src/main/java/tungtung/Task.java
+++ src/main/java/tungtung/Task.java
@@ -1,9 +1,11 @@
 package tungtung;
 
+/** Represents a task that can be marked as done or undone. */
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /** Creates an incomplete task with the supplied description. */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -13,10 +15,12 @@ public class Task {
         return (this.isDone ? "X" : " "); // mark done task with X
     }
 
+    /** Marks this task as completed. */
     public void setDone() {
         this.isDone = true;
     }
 
+    /** Marks this task as incomplete. */
     public void setUndone() {
         this.isDone = false;
     }
--- src/main/java/tungtung/ToDo.java
+++ src/main/java/tungtung/ToDo.java
@@ -1,6 +1,8 @@
 package tungtung;
 
+/** Represents a task without a date or time constraint. */
 public class ToDo extends Task {
+    /** Creates a todo task with the supplied description. */
     public ToDo(String description) {
         super(description);
     }
